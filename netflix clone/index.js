65e0f79ba2804979e1876d74e8321457f84de0fd
Moxh <moxhsharma878@gmail.com>
2025-06-26 12:43:18 +0530

    adding new project

--- netflix clone/index.js
+++ netflix clone/index.js
@@ -1,6 +1,1 @@
- const quetion = document.querySelector('.feq-box');
- quetion.forEach((quetion) => {
-    quetion.addEventListener('click', () => {
-        const answere = quetion.nextElementSibling; 
-        answere.style.display = answere.style.display === 'block' ? 'none' : 'block'})
-} );
+ 
