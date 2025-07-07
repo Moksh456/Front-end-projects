f6f5d04d481132ab6d266bde96fb07a51a9dfe69
Moxh <moxhsharma878@gmail.com>
2025-07-07 23:29:12 +0530

    dading new project

--- calculatopr app/script.js
+++ calculatopr app/script.js
@@ -1,8 +1,14 @@
 let calculator = document.querySelector(".calculator");
-let buttons = document.querySelectorAll(".buttons")
+let buttons = document.querySelectorAll("button");
+console.log(buttons)
 
 let input = document.querySelector("input")
 let String = "";
 
- 
+buttons.forEach(e => {
+        e.addEventListener("click",  ()=> {
+            
+        }
+        )
+})
 
