44bd13c432b11c16d4d5eaca6853e1c1c6bfe4c1
Moxh <moxhsharma878@gmail.com>
2025-07-15 23:43:48 +0530

    adding new js.

---
+++ spotify clone/script.js
@@ -0,0 +1,9 @@
+console.log("lets' weite a java script")
+
+async function main(){
+    let a = await fetch("C:\Users\USER\Desktop\Front-end-projects\spotify clone\songs")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div");
+    div.innerHTML = response;
+}
